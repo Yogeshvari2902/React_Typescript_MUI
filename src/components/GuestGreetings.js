77c8c47a589e3476f97e848e8ca63ab9a8f0ac0a
Yogeshvari2902 <yogeshvari2022@gmail.com>
2023-12-22 13:50:19 +0530

    react

--- src/components/GuestGreetings.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function GuestGreetings (props) {
-  return (
-    <div>GuestGreetings! Please sign up </div>
-  )
-}
--- src/components/UserGreetings.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function UserGreetings(props) {
-  return (
-    <div>UserGreetings! Welcome Back</div>
-  )
-}
